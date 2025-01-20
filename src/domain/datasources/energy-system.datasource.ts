1bf1a11adb625a6ac1add8f610e351d15d29f010
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-20 16:30:26 -0400

    [Domain]: Add dtos to domain datasources

--- src/domain/datasources/energy-system.datasource.ts
+++ src/domain/datasources/energy-system.datasource.ts
@@ -1,9 +1,14 @@
+import { PaginationDto } from "../../shared/dtos/pagination.dto";
+import { CreateEnergySystemDto } from "../dtos/energy-system/create-energy-system.dto";
+import { UpdateEnergySystemDto } from "../dtos/energy-system/update-energy-system.dto";
 import { EnergyGraph } from "../entities";
 
-export abstract class EnergyGraphDatasource {
+export abstract class EnergySystemDatasource {
   //Nodes
 
-  abstract createNode(createEnergyGraphDto: any): Promise<EnergyGraph>;
+  abstract createNode(
+    createEnergySystemDto: CreateEnergySystemDto
+  ): Promise<EnergyGraph>;
 
   abstract getAllNodes(paginationDto: any): Promise<EnergyGraph[]>;
 
@@ -11,14 +16,14 @@ export abstract class EnergyGraphDatasource {
 
   abstract updateNodeById(
     id: number,
-    updateEnergyGraphDto: any
+    updateEnergySystemDto: UpdateEnergySystemDto
   ): Promise<EnergyGraph>;
 
   abstract deleteNodeById(id: number): Promise<string>;
 
   abstract count(): Promise<number>;
 
-  abstract getRange(paginationDto: any): Promise<EnergyGraph[]>;
+  abstract getRange(paginationDto: PaginationDto): Promise<EnergyGraph[]>;
 
   abstract getByName(name: string): Promise<EnergyGraph | null>;
 
