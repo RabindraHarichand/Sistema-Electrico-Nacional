93d2d97edc175252ea8d2b5c4265ca378f0b472c
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-20 19:48:40 -0400

    [Domain]: Define User entity fromObject method

--- src/domain/entities/auth.entity.ts
+++ src/domain/entities/auth.entity.ts
@@ -1,4 +1,6 @@
-import { UserRole } from "../../shared/types/user.types";
+import { regularExps } from "../../shared/regular-expressions/regular-exp";
+import { userRole, UserRole } from "../../shared/types/user.types";
+import { CustomError } from "../errors/custom.error";
 
 export class AuthUser {
   constructor(
@@ -7,4 +9,25 @@ export class AuthUser {
     public readonly username: string,
     public readonly role: UserRole
   ) {}
+
+  public static fromObject(object: { [key: string]: any }): AuthUser {
+    const { id, email, role, username } = object;
+
+    if (!id) throw CustomError.badRequest("Missing id");
+
+    if (!email) throw CustomError.badRequest("Missing email");
+    if (!regularExps.email.test(email))
+      throw CustomError.badRequest("Email is not valid");
+
+    if (!role) throw CustomError.badRequest("Missing role");
+
+    if (!username) throw CustomError.badRequest("Missing username");
+
+    if (!userRole.includes(role as UserRole))
+      throw CustomError.badRequest(`Invalid role. Valid roles are ${userRole}`);
+
+    const user = new AuthUser(id, email, username, role as UserRole);
+
+    return user;
+  }
 }
--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,5 +1,7 @@
 import { UUID } from "crypto";
-import { UserRole } from "../../shared/types/user.types";
+import { userRole, UserRole } from "../../shared/types/user.types";
+import { CustomError } from "../errors/custom.error";
+import { regularExps } from "../../shared/regular-expressions/regular-exp";
 
 export class User {
   constructor(
@@ -12,4 +14,73 @@ export class User {
     public role: UserRole,
     public passwordHash: string
   ) {}
+
+  public static fromObject(object: { [key: string]: any }): User {
+    const {
+      id,
+      firstName,
+      lastName,
+      email,
+      passwordHash,
+      role,
+      username,
+      employeeCode,
+    } = object;
+
+    if (!id) throw CustomError.badRequest("Missing id");
+
+    if (!firstName) throw CustomError.badRequest("Missing first name");
+
+    if (!lastName) throw CustomError.badRequest("Missing last name");
+
+    if (!email) throw CustomError.badRequest("Missing email");
+    if (!regularExps.email.test(email))
+      throw CustomError.badRequest("Email is not valid");
+
+    if (!passwordHash) throw CustomError.badRequest("Missing password");
+
+    if (!role) throw CustomError.badRequest("Missing role");
+
+    if (!userRole.includes(role as UserRole))
+      throw CustomError.badRequest(`Invalid role. Valid roles are ${userRole}`);
+
+    if (!username) throw CustomError.badRequest("Missing username");
+    if (username.length > 12)
+      throw CustomError.badRequest(
+        "User name must be shortet than 12 characters"
+      );
+    if (username.length < 8)
+      throw CustomError.badRequest(
+        "User name must be longer than 8 characters"
+      );
+    if (!regularExps.atLeastOneDigit.test(username))
+      throw CustomError.badRequest("User name must have at least 1 digit");
+    if (!regularExps.atLeastOneSpecialChar.test(username))
+      throw CustomError.badRequest(
+        "User name must have at least 1 special character"
+      );
+    if (!regularExps.atLeastOneLowerCase.test(username))
+      throw CustomError.badRequest(
+        "User name must have at least 1 lowercase character"
+      );
+    if (!regularExps.atLeastOneUpperCase.test(username))
+      throw CustomError.badRequest(
+        "User name must have at least 1 uppercase character"
+      );
+
+    if (!employeeCode) throw CustomError.badRequest("Missing employeeCode");
+
+    const user = new User(
+      id,
+      firstName,
+      lastName,
+      email,
+      passwordHash,
+      employeeCode,
+      username,
+      role as UserRole
+    );
+
+    return user;
+  }
 }
