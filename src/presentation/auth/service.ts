7aa84b5dfe285214b9707605f3d04e1052d82348
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-20 20:02:13 -0400

    [Presentation]: Create Auth Service

---
+++ src/presentation/auth/service.ts
@@ -0,0 +1,64 @@
+import { bcryptAdapter } from "../../config/plugins/bcrypt.adapter";
+import { JwtAdapter } from "../../config/plugins/jwt.adapter";
+import { LoginUserDto } from "../../domain/dtos/auth/login-user.dto";
+import { CreateUserDto } from "../../domain/dtos/users/create-user.dto";
+import { User } from "../../domain/entities";
+
+import { CustomError } from "../../domain/errors/custom.error";
+import { UserRepository } from "../../domain/repositories/user.repository";
+export class AuthService {
+  constructor(private readonly repository: UserRepository) {}
+
+  public async login(loginUserDto: LoginUserDto) {
+    // Verifiy if user exists
+    const user = await this.repository.getByEmail(loginUserDto.email);
+    if (!user)
+      throw CustomError.badRequest(
+        `User with email ${loginUserDto.email} was not found`
+      );
+    //isMatch...bcrypt...compare(123456,sfaffaio3)
+    const isMatching = bcryptAdapter.compare(
+      loginUserDto.password,
+      user.passwordHash
+    );
+    if (!isMatching)
+      throw CustomError.badRequest("Password is not correct. Please try again");
+
+    const { passwordHash, ...userProps } = User.fromObject(user);
+
+    const token = await JwtAdapter.generateToken({
+      id: user.id,
+      email: user.email,
+    });
+    if (!token) throw CustomError.internalServer("Error while creating JWT");
+    return {
+      user: userProps,
+      token: token,
+    };
+  }
+
+  public async register(createUserDto: CreateUserDto) {
+    const existUser = await this.repository.getByEmail(createUserDto.email);
+    if (existUser) throw CustomError.badRequest("Email already exists");
+
+    try {
+      createUserDto.passwordHash = bcryptAdapter.hash(
+        createUserDto.passwordHash
+      );
+
+      const user = await this.repository.create(createUserDto);
+
+      const { passwordHash, ...userProps } = User.fromObject(user);
+      const token = await JwtAdapter.generateToken({
+        id: user.id,
+      });
+
+      return {
+        user: userProps,
+        token: token,
+      };
+    } catch (error) {
+      throw CustomError.internalServer(`${error}`);
+    }
+  }
+}
