b4ec991f68310ded385a669654433d90aaf6a675
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-22 10:44:09 -0400

    [Presentation]: Connect ActionLogRoutes with ActionLogController

--- src/presentation/action-logs/routes.ts
+++ src/presentation/action-logs/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { ActionLogController } from "./controller";
 
 export class ActionLogRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.get("/", () => {});
+    const actionLogController = new ActionLogController();
+    router.get("/", actionLogController.getAllUsers);
 
     return router;
   }
