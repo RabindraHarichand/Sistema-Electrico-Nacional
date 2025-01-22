44d13c06a62813ab0b8d1af994cc9cb105985726
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-22 10:54:05 -0400

    [Presentation]: Complete ActionLogRoutes

--- src/presentation/action-logs/controller.ts
+++ src/presentation/action-logs/controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { CustomError } from "../../domain/errors/custom.error";
 import { PaginationDto } from "../../shared/dtos/pagination.dto";
+import { ActionLogService } from "./service";
 
 export class ActionLogController {
-  constructor() {}
+  constructor(private readonly actionLogService: ActionLogService) {}
   private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError)
       return res.status(error.statusCode).json({ error: error.message });
@@ -17,5 +18,10 @@ export class ActionLogController {
     const { page = 1, limit = 10 } = req.query;
     const [error, paginationDto] = PaginationDto.create(+page, +limit);
     if (error) return res.status(400).json({ error });
+
+    this.actionLogService
+      .getAllLogs(paginationDto!)
+      .then((logs) => res.json(logs))
+      .catch((error) => this.handleError(error, res));
   };
 }
--- src/presentation/action-logs/routes.ts
+++ src/presentation/action-logs/routes.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { ActionLogController } from "./controller";
+import { ActionLogService } from "./service";
+import { ActionLogRepositoryImpl } from "../../infraestructure/repositories/action-log.repository.impl";
+import { ActionLogDatasourceIml } from "../../infraestructure/datasources/action-log.datasource.impl";
 
 export class ActionLogRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const actionLogController = new ActionLogController();
+    const actionLogDatasource = new ActionLogDatasourceIml();
+    const actionLogRepository = new ActionLogRepositoryImpl(
+      actionLogDatasource
+    );
+
+    const actionLogService = new ActionLogService(actionLogRepository);
+    const actionLogController = new ActionLogController(actionLogService);
+
     router.get("/", actionLogController.getAllLogs);
 
     return router;
