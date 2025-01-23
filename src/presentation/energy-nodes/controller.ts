1046ed154819ff538653c8f79d120d18a29b566f
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-23 17:04:27 -0400

    [Presentation]: Connect energy node controller with energy node service

--- src/presentation/energy-nodes/controller.ts
+++ src/presentation/energy-nodes/controller.ts
@@ -2,9 +2,10 @@ import { Request, Response } from "express";
 import { CustomError } from "../../domain/errors/custom.error";
 import { CreateEnergyNodeDto } from "../../domain/dtos/energy-system/energy-nodes/create-energy-node.dto";
 import { UpdateEnergyNodeDto } from "../../domain/dtos/energy-system/energy-nodes/update-energy-node.dto";
+import { EnergyNodeService } from "./service";
 
 export class EnergyNodeController {
-  constructor() {}
+  constructor(private readonly energyNodeService: EnergyNodeService) {}
 
   private handleError = (error: unknown, res: Response) => {
     if (error instanceof CustomError)
@@ -14,23 +15,43 @@ export class EnergyNodeController {
     return res.status(500).json({ error: "Internal server error" });
   };
 
-  public getAllNodes = async (req: Request, res: Response): Promise<any> => {};
+  public getAllNodes = async (req: Request, res: Response): Promise<any> => {
+    this.energyNodeService
+      .getAllNodes()
+      .then((nodes) => res.json(nodes))
+      .catch((error) => this.handleError(error, res));
+  };
 
   public createNode = async (req: Request, res: Response): Promise<any> => {
     const [error, createNodeDto] = CreateEnergyNodeDto.create(req.body);
     if (error) return res.status(400).json({ error });
+
+    this.energyNodeService
+      .createNode(createNodeDto!)
+      .then((node) => res.json(node))
+      .catch((error) => this.handleError(error, res));
   };
 
   public updateNode = async (req: Request, res: Response): Promise<any> => {
-    const id = req.params.id;
+    const id = +req.params.id;
     if (!id) return res.status(400).json({ error: "Missing id" });
 
     const [error, updateNodeDto] = UpdateEnergyNodeDto.create(req.body);
     if (error) return res.status(400).json({ error });
+
+    this.energyNodeService
+      .updateNode(id, updateNodeDto!)
+      .then((node) => res.json(node))
+      .catch((error) => this.handleError(error, res));
   };
 
   public deleteNode = async (req: Request, res: Response): Promise<any> => {
-    const id = req.params.id;
+    const id = +req.params.id;
     if (!id) return res.status(400).json({ error: "Missing id" });
+
+    this.energyNodeService
+      .deleteNode(id)
+      .then((node) => res.json(node))
+      .catch((error) => this.handleError(error, res));
   };
 }
