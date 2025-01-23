683ed53a11e728ac7fd96b0a77f24d7f07af0403
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-23 11:10:47 -0400

    [Presentation]: Connect Energy Link Controller with Energy Links Routes

--- src/presentation/energy-links/routes.ts
+++ src/presentation/energy-links/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
+import { EnergyLinkController } from "./controller";
 
 export class EnergyLinksRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.get("/", () => {}); //Obtener todos los nodos
-    router.post("/", () => {}); // Crear nodo
-    router.put("/:id", () => {}); //Actualizar nodo
-    router.delete("/:id", () => {}); //Borrar nodo
+    const energyLinkController = new EnergyLinkController();
+
+    router.get("/", energyLinkController.getAllLinks); //Obtener todos los nodos
+    router.post("/", energyLinkController.createLink); // Crear nodo
+    router.put("/:id", energyLinkController.updateLink); //Actualizar nodo
+    router.delete("/:id", energyLinkController.deleteLink); //Borrar nodo
 
     return router;
   }
