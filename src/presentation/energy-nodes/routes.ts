00b2490291b8a027550545e43e6e879bc46623d6
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-01-23 10:56:29 -0400

    [Presentation]: Connect Energy Node Controller with Energy Nodes Routes

--- src/presentation/energy-nodes/routes.ts
+++ src/presentation/energy-nodes/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
+import { EnergyNodeController } from "./controller";
 
 export class EnergyNodesRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.get("/", () => {}); //Obtener todos los nodos
-    router.post("/", () => {}); // Crear nodo
-    router.put("/:id", () => {}); //Actualizar nodo
-    router.delete("/:id", () => {}); //Borrar nodo
+    const energyNodeController = new EnergyNodeController();
+
+    router.get("/", energyNodeController.getAllNodes); //Obtener todos los nodos
+    router.post("/", energyNodeController.createNode); // Crear nodo
+    router.put("/:id", energyNodeController.updateNode); //Actualizar nodo
+    router.delete("/:id", energyNodeController.deleteNode); //Borrar nodo
 
     return router;
   }
